e662d611397fa9c66549658ac1f7aca88c3b2cb9
fishfishteng <www.elmo20816@gmail.com>
2016-03-15 22:02:28 +0800

    lab1_B

---
+++ lab1_B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int n;
+
+	cin >> n;
+	cout << endl;
+	cout << n << " ";
+	while(n != 1){ 
+		if(n % 2 == 1){
+			n = 3*n + 1;
+			cout << n << " ";
+		}else{
+			n /= 2;
+			cout << n << " ";
+		}
+	}
+	cout << endl;
+	return 0;
+} 
